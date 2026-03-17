59c73236489b47e058364c20e99d0e652be4f0ee
Lan Lan <llanlan91@gmail.com>
2026-03-17 19:00:31 +0800

    Add security group for NGINX web server

--- main.tf
+++ main.tf
@@ -8,9 +8,43 @@ data "aws_ami" "latest_amazon_linux" {
   }
 }
 
+resource "aws_security_group" "web_sg" {
+  name        = "web-server-sg"
+  description = "Security group for NGINX web server"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow HTTP traffic from anywhere"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR_IP/32"]
+    description = "Allow SSH from my IP only"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow all outbound traffic"
+  }
+
+  tags = {
+    Name = "web-server-sg"
+  }
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.latest_amazon_linux.id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
 
   tags = {
     Name = "Terraform-Web-Server"
